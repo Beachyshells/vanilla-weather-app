4bdf1ee5dca40f17ee9cfd6de8a35350a11c7218
Beachyshells <5beachflower5@gmail.com>
2025-05-26 15:41:42 -0400

    added axios and button function

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,17 @@
-function showCity() {
+function showCity(event) {
+  event.preventDefault();
   let city = document.querySelector("#search-city").value;
   let h2 = document.querySelector("h2");
   h2.innerHTML = city;
 }
 let button = document.querySelector("#iniate-search");
 button.addEventListener("click", showCity);
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=b9aaeaaf97004f2a03afob830bt63baf&units=imperial`;
+let apiKey = "b9aaeaaf97004f2a03afob830bt63baf";
+axios.get(
+  `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`
+);
+axios.get(apiUrl).then(response);
+
+response = response.data;
+console.log(response.data);
